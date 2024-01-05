0704bc92fd45949f1576a034846c32209b58c2ad
TsogtSaruul <tsogtsaruul@gmail.com>
2024-01-05 20:59:03 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { 
+    BrowserRouter as Router, 
+    Routes, 
+    Route, 
+    Navigate 
+} from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth";
+import PostDetails from './components/PostDetails/PostDetails';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+    const user = JSON.parse(localStorage.getItem('profile'));
+
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="/auth" element={
+                        !user ? <Auth /> : <Navigate to="/" />} />
+                    <Route path="/posts/search" element={<Home />} />
+                    <Route path="/posts/:id" element={<PostDetails />} />
+                </Route> 
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { 
+    legacy_createStore as createStore, 
+    compose, 
+    applyMiddleware 
+} from 'redux';
+import reducers from './reducers';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
